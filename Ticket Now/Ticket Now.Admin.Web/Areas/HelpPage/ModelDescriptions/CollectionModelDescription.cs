2e84e54e9f1c7d3afb788760cf2990845f37387f
Jake Daryl Moresca <jmoresca@outlook.com>
2015-09-13 03:23:32 +0000

    working login with correct libraries Imported from Changeset 109

--- Ticket Now/Ticket Now.Admin.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Ticket_Now.Admin.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Ticket Now/Ticket Now.Admin.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Ticket_Now.Admin.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
